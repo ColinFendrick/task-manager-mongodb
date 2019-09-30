1df3de8ac094808e2042aced2e88f7816a0be581
Colin Fendrick <colin.fendrick@gmail.com>
2019-09-30 11:07:34 -0400

    Add some simple insertmany calls

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,48 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    name: 'Colin',
-    age: 29
+  // db.collection('users').insertOne({
+  //   name: 'Colin',
+  //   age: 29
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Jen',
+  //     age: 28
+  //   },
+  //   {
+  //     name: 'Gunther',
+  //     age: 27
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Get groceries',
+      completed: true
+    },
+    {
+      description: 'Do laundry',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) { return console.log('Cannot insert tasks', error) }
+
+    console.log(result.ops)
   })
+
+  client.close()
 })
