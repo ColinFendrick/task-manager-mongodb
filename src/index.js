fb10e6bfa911ce393821ca597255fcbef610eaeb
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-01 11:03:40 -0400

    Add read tasks endpoints

--- src/index.js
+++ src/index.js
@@ -9,17 +9,55 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-  const user = new User(req.body)
-
-  user.save()
-    .then(user => res.send(user))
+  new User(req.body).save()
+    .then(user => res.status(201).send(user))
     .catch(err => res.status(400).send(err))
+})
+
+app.get('/users', (req, res) => {
+  User.find()
+    .then(users => {
+      if (!users) { return res.status(404).send() }
+
+      res.send(users)
+    })
+    .catch(err => res.status(500).send(err))
+})
+
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) { return res.status(400).send() }
+
+      res.send(user)
+    })
+    .catch(err => res.status(00).send(err))
 })
 
 app.post('/tasks', (req, res) => {
   new Task(req.body).save()
-    .then(task => res.send(task))
-    .catch(err => res.status(400).send(err))
+    .then(task => res.status(201).send(task))
+    .catch(err => res.status(404).send(err))
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find()
+    .then(tasks => {
+      if (!tasks) { return res.status(404).send() }
+
+      res.send(tasks)
+    })
+    .catch(err => res.status(500).send(err))
+})
+
+app.get('/tasks/:id', (req, res) => {
+  Task.findById(req.params.id)
+    .then(task => {
+      if (!task) { return res.status(404).send() }
+
+      res.send(task)
+    })
+    .catch(err => res.status(500).send(err))
 })
 
 app.listen(port, () => {
