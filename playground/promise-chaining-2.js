65cdef0adc0555cf3181ef2be4041ab1df905ac0
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-01 14:49:21 -0400

    Turn the chains into async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,7 +1,16 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5d933d4e639d705567579197')
-  .then(() => Task.countDocuments({ completed: false }))
+// Task.findByIdAndDelete('5d933d4e639d705567579197')
+//   .then(() => Task.countDocuments({ completed: false }))
+//   .then(res => console.log(res))
+//   .catch(e => console.log(e))
+
+const deleteTaskAndCount = async id => {
+  await Task.findByIdAndDelete(id)
+  return await Task.countDocuments({ completed: false })
+}
+
+deleteTaskAndCount('5d9366817eed5b7b27b45d75')
   .then(res => console.log(res))
   .catch(e => console.log(e))
