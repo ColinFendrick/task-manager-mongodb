516b62ca2070a3d56a29e4ca1d875d2d33ed67ce
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-01 07:43:11 -0400

    Remove gitignore files

---
+++ src/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
+
+mongoose.connect(connectionURL, { useNewUrlParser: true, useCreateIndex: true })
+
+const User = mongoose.model('User', {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+})
