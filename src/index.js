7782cbce44ed050ae34c1ff8c8db3f0b9c573bc2
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-02 07:18:23 -0400

    Add patch endpoint for user

--- src/index.js
+++ src/index.js
@@ -8,56 +8,85 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
-  new User(req.body).save()
-    .then(user => res.status(201).send(user))
-    .catch(err => res.status(400).send(err))
+app.post('/users', async (req, res) => {
+  const user = new User(req.body)
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-app.get('/users', (req, res) => {
-  User.find()
-    .then(users => {
-      if (!users) { return res.status(404).send() }
+app.get('/users', async (req, res) => {
+  try {
+    const user = await User.find()
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
 
-      res.send(users)
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) { return res.status(400).send() }
+
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, runValidators: true
     })
-    .catch(err => res.status(500).send(err))
+
+    if (!user) { return res.status(404).send() }
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-app.get('/users/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then(user => {
-      if (!user) { return res.status(400).send() }
-
-      res.send(user)
-    })
-    .catch(err => res.status(00).send(err))
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body)
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
-app.post('/tasks', (req, res) => {
-  new Task(req.body).save()
-    .then(task => res.status(201).send(task))
-    .catch(err => res.status(404).send(err))
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.send(tasks)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
-app.get('/tasks', (req, res) => {
-  Task.find()
-    .then(tasks => {
-      if (!tasks) { return res.status(404).send() }
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id)
+    if (!task) { return res.status(404).send() }
 
-      res.send(tasks)
-    })
-    .catch(err => res.status(500).send(err))
-})
-
-app.get('/tasks/:id', (req, res) => {
-  Task.findById(req.params.id)
-    .then(task => {
-      if (!task) { return res.status(404).send() }
-
-      res.send(task)
-    })
-    .catch(err => res.status(500).send(err))
+    res.send(task)
+  } catch (e) {
+    res.status(500).send(e)
+  }
 })
 
 app.listen(port, () => {
