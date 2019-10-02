7197098a23cc533ac1493abc3fa509935aa7427d
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-02 07:42:18 -0400

    Add delete methods

--- src/index.js
+++ src/index.js
@@ -59,6 +59,16 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) { res.status(404).send() }
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
   try {
@@ -86,6 +96,34 @@ app.get('/tasks/:id', async (req, res) => {
     res.send(task)
   } catch (e) {
     res.status(500).send(e)
+  }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOp = updates.every(update => allowedUpdates.includes(update))
+  if (!isValidOp) { return res.status(400).send({ error: 'Update is invalid'}) }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, runValidators: true
+    })
+    if (!task) { return res.status(404).send() }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if (!task) { res.status(404).send() }
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
   }
 })
 
